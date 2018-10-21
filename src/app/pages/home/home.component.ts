4db8571b5abbd6c0a9cadfa9b0f39d34b049a9b2
Abby Zhang <abbyzhang@Abbys-MBP.home>
2018-10-20 17:46:25 -1000

    📖  DOC: home-page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home-page',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  title: string = 'ng-rolodex- Home Page';
   constructor() { }
 
   ngOnInit() {
